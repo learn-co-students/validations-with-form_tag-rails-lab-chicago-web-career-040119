37cef51fda0cb0aeaa414513949be511bd0d08b3
Jackson Robbins <jackrobbins1@gmail.com>
2019-05-01 15:42:01 -0500

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,6 +1,6 @@
 class AuthorsController < ApplicationController
   def show
-    @author = Author.find(params[:id])
+    set_author
   end
 
   def new
@@ -8,12 +8,34 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create(author_params)
+    @author = Author.new(author_params)
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else
+      @errors = @author.errors.full_messages
+      render :new
+    end
+  end
 
-    redirect_to author_path(@author)
+  def edit
+    set_author
+  end
+
+  def update
+    set_author
+    if @author.update(author_params)
+      redirect_to author_path(@author)
+    else
+      render :edit
+    end
   end
 
   private
+
+  def set_author
+    @author = Author.find(params[:id])
+  end
 
   def author_params
     params.permit(:name, :email, :phone_number)
